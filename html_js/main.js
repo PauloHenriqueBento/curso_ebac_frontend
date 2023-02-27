42822d0aef1b43a85754c1aca562dadf94240ca3
PauloHenriqueBentoDosSantos <phbento@outlook.com.br>
2023-02-27 01:43:22 -0300

    Fix: convertido valores do input para number

--- html_js/main.js
+++ html_js/main.js
@@ -7,31 +7,41 @@ const button = document.getElementById('submit')
 
 let formValido = false;
 
-function validaValores(a, b) {
-    a && b ? button.classList.add('enviar') : button.classList.remove('enviar');
-    return b > a ? formValido = true : formValido = false;
+function validaValores() {
+    valorA.value && valorB.value ? button.classList.add('enviar') : button.classList.remove('enviar');
+
+    let a = Number(valorA.value);
+    let b = Number(valorB.value);
+
+    if (b > a) {
+        return true
+    } else {
+        return false
+    } 
 }
 
 form.addEventListener('submit', e => {
     e.preventDefault();
+    formValido = validaValores();
 
-    if (validaValores(valorA.value, valorB.value)) {
+    if (formValido) {
         aviso.classList.add('success')
-        aviso.innerHTML = 'B é maior que A'
+        aviso.innerHTML = `Valor B: ${valorB.value} é maior que Valor A: ${valorA.value}`
 
         valorA.value = '';
         valorB.value = '';
     } else {
         aviso.classList.add('error')
-        aviso.innerHTML = 'Não posso validar'
+        aviso.innerHTML = `Não posso validar. Valor A: ${valorA.value} é maior que Valor B: ${valorB.value}`
     }
 });
 
 form.addEventListener('keyup', e => {
-    formValido = false;
-    if (!validaValores(valorA.value, valorB.value)) {
+    formValido = validaValores();
+
+    if (!formValido) {
         aviso.classList.add('error')
-        aviso.innerHTML = 'A é maior que B, forma invalida'
+        aviso.innerHTML = `Valor A: ${valorA.value} é maior que Valor B: ${valorB.value}, forma invalida`
     } else {
         aviso.classList.remove('error')
         aviso.innerHTML = ''
