b06979d9feb9f4a33616f18c7179c12c28eb25f3
PauloHenriqueBentoDosSantos <phbento@outlook.com.br>
2024-12-02 02:06:06 -0300

    exercicio finalizado

---
+++ index.js
@@ -0,0 +1,10 @@
+function multiply(a, b) {
+    return a * b;
+}
+function greet(name) {
+    return "Ol\u00E1 ".concat(name);
+}
+var result = multiply(4, 5);
+var greeting = greet("João");
+console.log(result);
+console.log(greeting);
---
+++ index.ts
@@ -0,0 +1,12 @@
+function multiply(a: number, b: number): number {
+    return a * b;
+}
+function greet(name: string): string {
+    return `Olá ${name}`;
+}
+
+const result = multiply(4, 5);
+const greeting = greet("João")
+
+console.log(result);
+console.log(greeting);
